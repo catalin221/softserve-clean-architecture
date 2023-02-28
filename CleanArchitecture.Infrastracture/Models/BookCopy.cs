2fc99bad86ecd737ad6e944e6b6e484891acd482
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2023-02-28 21:28:32 +0200

    BookCopy, Borrowing models

---
+++ CleanArchitecture.Infrastracture/Models/BookCopy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastracture.Models
+{
+    public class BookCopy
+    {
+        [Required]
+        public int Id { get; private set; }
+
+        [Required]
+        public int? BookId { get; private set; }
+
+        [Required]
+        public Book? Book { get; private set; }
+
+        [Required]
+        public bool? IsBorrowed { get; private set; }
+    }
+}
---
+++ CleanArchitecture.Infrastracture/Models/Borrowing.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchitecture.Infrastracture.Models
+{
+    public class Borrowing
+    {
+        [Required]
+        public int Id { get; private set; }
+        [Required]
+        public int? UserId { get; private set; }
+        [Required]
+        public User? User { get; private set; }
+        [Required]
+        public int? BookCopyId { get; private set; }
+        [Required]
+        public BookCopy? BookCopy { get; private set; }
+        public DateTime? StartDate { get; private set; }
+        public DateTime? EndDate { get; private set; }
+        public DateTime? TimeSpent { get; private set; }
+    }
+}
